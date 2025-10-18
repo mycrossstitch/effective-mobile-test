072febc6e389efe59e68c1d66348be58abd88e14
Olga <mycrossstitch@yandex.ru>
2025-10-18 16:42:58 +0300

    Локаторы и cсылки вынесены в отдельный класс class MainPageLocators() и отдельный файл locators.py в main_page реализован выбор локаторов в зависимости от видимости бургер-меню изменена логика check_all_links - LINKS, а не локаторы

---
+++ pages/locators.py
@@ -0,0 +1,45 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+
+    LOCATORS_FULL_MENU = {
+        "logo_header": (By.XPATH, "(//a[@href='#main' and text()='Effective Mobile'])[1]"),
+        "logo_footer": (By.XPATH, "(//a[@href='#main' and text()='Effective Mobile'])[2]"),
+        "about": (By.XPATH, "//a[@href='#about' and text()='[ О нас ]']"),
+        "services": (By.XPATH, "//a[@href='#moreinfo' and text()='[ Услуги ]']"),
+        "cases": (By.XPATH, "//a[@href='#cases' and text()='[ Проекты ]']"),
+        "Reviews": (By.XPATH, "//a[@href='#Reviews' and text()='[ Отзывы ]']"),
+        "contacts": (By.XPATH, "//a[@href='#contacts' and text()='[ Контакты ]']"),
+        "specialists": (By.XPATH, "//a[@href='#specialists']"),
+        "cooperation": (By.XPATH, "(//a[@href='#contacts'])[3]"),
+        "moreinfo": (By.XPATH, "//a[@href='#moreinfo' and text()='Подробнее']")
+    }
+
+    LOCATORS_BURGER_MENU = {
+        "burger_button" : (By.CSS_SELECTOR, "button.t-menuburger.t-menuburger_first"),
+        "container" : (By.CLASS_NAME, "t450__container"),
+        "logo_header": (By.CSS_SELECTOR, ".t450__logo.t-heading.t-heading_xs"),
+        "logo_footer": (By.XPATH, "(//a[@href='#main' and text()='Effective Mobile'])[2]"),
+        "about": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#about']"),
+        "services": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#moreinfo']"),
+        "cases": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#cases']"),
+        "Reviews": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#Reviews']"),
+        "contacts": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#contacts']"),
+        "specialists": (By.XPATH, "//a[@href='#specialists']"),
+        "cooperation": (By.XPATH, "(//a[@href='#contacts'])[3]"),
+        "moreinfo": (By.XPATH, "//a[@href='#moreinfo' and text()='Подробнее']"),
+
+    }
+
+    LINKS = {
+        "logo_header": "/#main",
+        "logo_footer": "/#main",
+        "about": "/#about",
+        "services": "/#moreinfo",
+        "cases": "/#cases",
+        "Reviews": "/#Reviews",
+        "contacts": "/#contacts",
+        "specialists": "/#specialists",
+        "cooperation": "/#contacts",
+        "moreinfo": "/#moreinfo",
+    }
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,43 +1,26 @@
 
 import allure
 from pages.base_page import BasePage
-from selenium.webdriver.common.by import By
+from pages.locators import MainPageLocators as MPL
 
 class MainPage(BasePage):
 
     PAGE_URL = 'https://effective-mobile.ru'
 
-    LOCATORS = {
-        "logo_header": (By.XPATH, "(//a[@href='#main' and text()='Effective Mobile'])[1]"),
-        "logo_footer": (By.XPATH, "(//a[@href='#main' and text()='Effective Mobile'])[2]"),
-        "about": (By.XPATH, "//a[@href='#about' and text()='[ О нас ]']"),
-        "services" : (By.XPATH, "//a[@href='#moreinfo' and text()='[ Услуги ]']"),
-        "cases": (By.XPATH, "//a[@href='#cases' and text()='[ Проекты ]']"),
-        "Reviews" : (By.XPATH, "//a[@href='#Reviews' and text()='[ Отзывы ]']"),
-        "contacts": (By.XPATH, "//a[@href='#contacts' and text()='[ Контакты ]']"),
-        "specialists": (By.XPATH, "//a[@href='#specialists']"),
-        "cooperation": (By.XPATH, "(//a[@href='#contacts'])[3]"),
-        "moreinfo": (By.XPATH, "//a[@href='#moreinfo' and text()='Подробнее']")
-    }
+    def get_locators(self):
+        """Выбираем текущий набор локаторов в зависимости от наличия бургер-меню"""
+        if self.is_element_visible(MPL.LOCATORS_BURGER_MENU["burger_button"]):
+            return MPL.LOCATORS_BURGER_MENU
+        return MPL.LOCATORS_FULL_MENU
 
-    LINKS = {
-        "logo_header": "https://effective-mobile.ru/#main",
-        "logo_footer": "https://effective-mobile.ru/#main",
-        "about": "https://effective-mobile.ru/#about",
-        "services": "https://effective-mobile.ru/#moreinfo",
-        "cases": "https://effective-mobile.ru/#cases",
-        "Reviews": "https://effective-mobile.ru/#Reviews",
-        "contacts": "https://effective-mobile.ru/#contacts",
-        "specialists": "https://effective-mobile.ru/#specialists",
-        "cooperation": "https://effective-mobile.ru/#contacts",
-        "moreinfo": "https://effective-mobile.ru/#moreinfo",
-    }
+
 
     @allure.step("Проверка всех локаторов на главной странице")
     def check_all_locators_exist(self):
         errors = []  # сюда будем собирать сообщения об ошибках
+        locators = self.get_locators()
 
-        for name, locator in self.LOCATORS.items():
+        for name, locator in locators.items():
             with allure.step(f"Проверяем наличие элемента: {name}"):
                 if not self.is_element_present(locator):
                     message = f"❌ Элемент '{name}' не найден. Локатор: {locator}"
@@ -58,23 +41,32 @@ class MainPage(BasePage):
     @allure.step("Проверка корректности всех ссылок на главной странице")
     def check_all_links(self):
         errors = []
+        locators = self.get_locators()
 
-        for name, locator in self.LOCATORS.items():
-            expected_url = self.LINKS.get(name)
+        # Перебираем ссылки по ключу
+        for name, relative_url in MPL.LINKS.items():
+            locator = locators.get(name)
+            expected_url = f"{self.PAGE_URL}{relative_url}"
+
             with allure.step(f"Проверяем элемент: {name}"):
+                # Проверяем наличие локатора
+                if not locator:
+                    errors.append(f"⚠️ Для '{name}' нет локатора в LOCATORS")
+                    continue
+
+                # Проверяем, что элемент есть на странице
                 if not self.is_element_present(locator):
                     errors.append(f"❌ Элемент '{name}' отсутствует на странице")
                     continue
 
                 try:
-
-                    # Кликаем через уже готовый метод
+                    # Кликаем и проверяем переход
                     self.click_element(locator)
                     self.verify_current_url(expected_url)
-
                 except AssertionError as e:
                     errors.append(f"{name}: {e}")
                 finally:
+                    # Возврат на главную
                     self.driver.back()
 
         if errors:
