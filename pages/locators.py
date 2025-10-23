c8f8d2291fc005ccb69245d7c47411744123e0ce
Olga <mycrossstitch@yandex.ru>
2025-10-23 14:52:59 +0300

    улучшены локаторы

--- pages/locators.py
+++ pages/locators.py
@@ -3,29 +3,29 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
 
     LOCATORS_FULL_MENU = {
-        "logo_header": (By.XPATH, "(//a[@href='#main' and text()='Effective Mobile'])[1]"),
-        "about": (By.XPATH, "//a[@href='#about' and text()='[ О нас ]']"),
-        "services": (By.XPATH, "//a[@href='#moreinfo' and text()='[ Услуги ]']"),
-        "cases": (By.XPATH, "//a[@href='#cases' and text()='[ Проекты ]']"),
-        "Reviews": (By.XPATH, "//a[@href='#Reviews' and text()='[ Отзывы ]']"),
-        "contacts": (By.XPATH, "//a[@href='#contacts' and text()='[ Контакты ]']"),
-        "specialists": (By.XPATH, "//a[@href='#specialists']"),
+        "logo_header": (By.CSS_SELECTOR, "#rec573054532 a[href='#main']"),
+        "about": (By.CSS_SELECTOR, "#rec573054532 a[href='#about']"),
+        "services": (By.CSS_SELECTOR, "#rec573054532 a[href='#moreinfo']"),
+        "cases": (By.CSS_SELECTOR, "#rec573054532 a[href='#cases']"),
+        "reviews": (By.CSS_SELECTOR, "#rec573054532 a[href='#Reviews']"),
+        "contacts": (By.CSS_SELECTOR, "#rec573054532 a[href='#contacts']"),
+        "specialists": (By.CSS_SELECTOR, "#rec573054532 a[href='#specialists']"),
     }
 
     LOCATORS_BURGER_MENU = {
-        "burger_button" : (By.CSS_SELECTOR, "button.t-menuburger.t-menuburger_first"),
-        "logo_header": (By.CSS_SELECTOR, ".t450__logo.t-heading.t-heading_xs"),
-        "about": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#about']"),
-        "services": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#moreinfo']"),
-        "cases": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#cases']"),
-        "Reviews": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#Reviews']"),
-        "contacts": (By.CSS_SELECTOR, "a.t-menu__link-item[href='#contacts']"),
+        "burger_button" : (By.CSS_SELECTOR, "button.t-menuburger_first"),
+        "logo_header": (By.CSS_SELECTOR, ".t450__container .t450__logo.t-heading"),
+        "about": (By.CSS_SELECTOR, ".t450__container a[href='#about']"),
+        "services": (By.CSS_SELECTOR, ".t450__container a[href='#moreinfo']"),
+        "cases": (By.CSS_SELECTOR, ".t450__container a[href='#cases']"),
+        "reviews": (By.CSS_SELECTOR, ".t450__container a[href='#Reviews']"),
+        "contacts": (By.CSS_SELECTOR, ".t450__container a[href='#contacts']"),
     }
 
     LOCATORS_PAGE = {
-        "cooperation": (By.XPATH, "(//a[@href='#contacts'])[3]"),
-        "moreinfo": (By.XPATH, "//a[@href='#moreinfo' and text()='Подробнее']"),
-        "logo_footer": (By.XPATH, "(//a[@href='#main' and text()='Effective Mobile'])[2]"),
+        "cooperation": (By.CSS_SELECTOR, "#sbs-572374517-1680509731311 a[href='#contacts']"),
+        "moreinfo": (By.CSS_SELECTOR, "#sbs-571993583-1680426641483 a[href='#moreinfo']"),
+        "logo_footer": (By.CSS_SELECTOR, "#rec572471347 a[href='#main']"),
     }
 
     MENU_LINKS = {
@@ -33,7 +33,7 @@ class MainPageLocators():
         "about": "/#about",
         "services": "/#moreinfo",
         "cases": "/#cases",
-        "Reviews": "/#Reviews",
+        "reviews": "/#Reviews",
         "contacts": "/#contacts",
         "specialists": "/#specialists",  # ⚠️ отсутствует на странице с бургер-меню (ожидаемое поведение/баг)
     }
